625969fc1fe36482bb5efe2d120b42b1e9bc3853
xiaoj <xiaojin1992@126.com>
2022-08-04 16:28:55 +0000

    feat: it worth

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    watch: {
+      ignored: [
+        "!**/node_modules/react-router/**",
+        "!**/node_modules/react-router-dom/**",
+      ],
+    },
+  },
+  optimizeDeps: {
+    exclude: ["react-router", "react-router-dom"],
+  },
+});
